4382fd320732542de877a24776519b3f052e5bbc
Peter Murphy <peter.francis.murphy@gmail.com>
2019-04-03 11:39:43 -0400

    Add working form

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ module Main where
 import           Control.Monad          (forM_)
 import           Control.Monad.IO.Class (liftIO)
 import           Data.IORef
+import           Data.Semigroup         ((<>))
 import           Data.Text              (Text)
 import           Lucid
 import           Web.Spock
@@ -23,7 +24,7 @@ newtype ServerState = ServerState
 type Server a = SpockM () () ServerState a
 
 app :: Server ()
-app =
+app = do
   get root $ do
     ns' <- getState >>= (liftIO . readIORef . notes)
     lucid $ do
@@ -34,6 +35,21 @@ app =
             toHtml (author n)
             ": "
             toHtml (contents n)
+      h2_ "New note"
+      form_ [method_ "post"] $ do
+        label_ "Name"
+        input_ [type_ "text", name_ "author"]
+        label_ "Contents"
+        input_ [type_ "text", name_ "contents"]
+        button_ "Submit"
+  post root $ do
+    author <- param' "author"
+    contents <- param' "contents"
+    notesRef <- notes <$> getState
+    liftIO $
+      atomicModifyIORef' notesRef $ \notes ->
+        (notes <> [Note author contents], ())
+    redirect "/"
 
 main :: IO ()
 main = do
